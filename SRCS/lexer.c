36389afaf307373c589efa7ced3cf9966cdbc615
Tanguy Gellon <tgellon@z4r10p5.42lyon.fr>
2023-03-30 14:37:11 +0200

    lexer OK pour words et tokens ; gerer les quotes

--- SRCS/lexer.c
+++ SRCS/lexer.c
@@ -6,77 +6,84 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 13:45:15 by tgellon           #+#    #+#             */
-/*   Updated: 2023/03/29 17:13:57 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/03/30 11:05:29 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
 
-t_lexer	*new_lexer_node(t_lexer *lexer, char *str, int token)
+static int	spaces_skip(char *str, int i)
 {
-	t_lexer	*node;
+	int	j;
 
-	node = (t_lexer *)malloc(sizeof(t_lexer));
-	if (!node)
-		return (NULL);
-	if (!node->prev)
-		node->index = 0;
-	else
-		node->index = node->prev->index + 1;
-	if (token == 0)
-	{
-		node->word.name = str;
-		node->token.name = NULL;
-	}
-	else
-	{
-		node->word.name = NULL;
-		node->token.name = str;
-	}
-	node->next = NULL;
-	return (node);
+	j = 0;
+	while (ft_isspace(str[i + j]))
+		j++;
+	return (j);
 }
 
-void	*add_node(t_lexer *lexer, char *str, int token)
+static int	is_token(char *str, int i)
 {
-	t_lexer	*new;
-	t_lexer	*tmp;
+	if (str[i] == '|' || str[i] == '<' || str[i] == '>')
+		return ((int)str[i]);
+	return (0);
+}
 
-	new = new_lexer_node(lexer, str, token);
-	if (!new)
-		return (NULL);
-	if (!lexer)
+static int	tokenizer(t_data *data, char *str, int i)
+{
+	char	*token;
+
+	if ((str[i] == '>' && str[i + 1] == '>') \
+		|| (str[i] == '<' && str[i + 1] == '<'))
 	{
-		lexer = new;
-		return ;
+		token = ft_substr(str, i, 2);
+		if (!add_node(data->lexer, token, 1))
+			return (-1);
+		free(token);
+		return (2);
 	}
-	tmp = lexer;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
+	else
+	{
+		token = ft_substr(str, i, 1);
+		if (!add_node(data->lexer, token, 1))
+			return (-1);
+		free(token);
+		return (1);
+	}
+}
+
+static int	worder(t_data *data, char *str, int i)
+{
+	char	*new;
+	int		j;
+
+	j = 0;
+	while (str[i + j] != '\0' && !ft_isspace(str[i+j]) && !is_token(str, i))
+		j++;
+	new = ft_substr(str, i, j);
+	if (!add_node(data->lexer, str, 0))
+		return (-1);
+	free(new);
+	return (j);
 }
 
 int	lexer_init(t_data *data)
 {
 	int		i;
 	int		j;
-	char	**words;
 
 	i = 0;
-	words = ft_split(data->str, ' ');
-	if (!words)
-		;
-	while (words[i])
+	while (data->str[i])
 	{
 		j = 0;
-		while (data->tokens_tab[j])
-		{
-			if (ft_strncmp(words[i], data->tokens_tab[j], ft_strlen(words[i])) == 0)
-				add_node(data->lexer, words[i], 1);
-			else
-				j++;
-		}
-		add_node(data->lexer, words[i], 0);
-		i++;
+		i = i + spaces_skip(data->str, i);
+		if (is_token(data->str, i))
+			j = tokenizer(data, data->str, i);
+		else
+			j = worder(data, data->str, i);
+		if (j == -1)
+			return (0);
+		i = i + j;
 	}
+	return (1);
 }
