b363aa9daba6770a07029590580c01f4fbe0e499
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-04-25 16:46:49 +0200

    correcting expand

--- SRCS/expand.c
+++ SRCS/expand.c
@@ -3,37 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 09:45:08 by tgellon           #+#    #+#             */
-/*   Updated: 2023/04/25 09:45:47 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/04/25 16:45:51 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
 //Attention si hdh"$USER"hdg -> 1 seul node donc quotes pas au début et à la fin
-//ls | "'$USER'" "user is $USER" "$USER $USER"
+//ls | "'$USER'" "user is $USER" "$USER $USER"     $USE'R'"$USER $USER"$USER
 static char	*expand_str(t_data *data, char *s) //func too long
 {
 	size_t	j;
 	char	*bef;
 	char	*exp;
 	char	*aft;
-
+s = str_quotes_removal(s);
 	bef = NULL;
 	exp = NULL;
 	aft = NULL;
 	j = 0;
 	while (s[j] != '\0')
 	{
-		if (s[j] == '$' && s[j + 1] != ' ' && ft_isprint(s[j + 1]) == 1 && \
-		s[j + 1] != '\0')
+		if (s[j] == '$' && (ft_isalnum(s[j + 1]) == 1 || s[j + 1] == '?'))
 		{
+			j++;
 			if (j > 0)
 				bef = ft_substr(s, 0, j);
-// if (bef != NULL)
-	// printf("\n\n\nbef = %s c = %d\n", bef, bef[ft_strlen(bef) - 1]);
-			while (s[j] != ' ' && s[j] != '\0' && s[j] != '\'')
+if (bef != NULL)
+	printf("\n\n\nbef = %s c = %d\n", bef, bef[ft_strlen(bef) - 1]);
+			while (ft_isalnum(s[j]) == 1 || s[j] == '?')//faire j = fonction et bien checker dans new func
 				j++;//ou fonc si 0 alors ++ sinon break et chercher aussi $
 				// if (s[j] == '$' && s[j - 1] && s[j - 1] != '$')
 				// 	break ;
@@ -41,11 +41,11 @@ static char	*expand_str(t_data *data, char *s) //func too long
 
 
 
-// printf("char stopped: %c\n", s[j]);
+printf("j: %lu\n", j);
 			aft = ft_substr(s, j, ft_strlen(s));
-// printf("aft = %s c = %d\n", aft, aft[ft_strlen(aft) - 1]);
-			exp = ft_substr(s, ft_strlen(bef) + 1, j - ft_strlen(bef) - 1);
-// printf("exp = %s c = %d\n\n\n", exp, exp[ft_strlen(exp) - 1]);
+printf("aft = %s c = %d\n", aft, aft[ft_strlen(aft) - 1]);
+			exp = ft_substr(s, ft_strlen(bef), j - ft_strlen(bef));
+printf("exp = %s c = %d\n\n\n", exp, exp[ft_strlen(exp) - 1]);
 			// s = get_var()
 			exp = get_var(data, exp);
 			j = ft_strlen(bef) + ft_strlen(exp);
@@ -56,49 +56,56 @@ static char	*expand_str(t_data *data, char *s) //func too long
 	return (s);
 }
 
-static char	*expand_d_quotes(t_data *data, char *s, size_t *i)
+static char	*expand_quotes(t_data *data, char *s, size_t *i, char c)
 {
 	char	*str_b;
 	char	*str_m;
 	char	*str_a;
-	size_t	j;
 
 	str_b = NULL;
 	str_m = NULL;
 	str_a = NULL;
 	if (*i > 0)
 		str_b = ft_substr(s, 0, *i);
-	j = *i + 1;
-	while (s[j] != '"')
-		j++;
-	str_m = ft_substr(s, *i + 1, j - *i - 1);
-	if (j + 1 < ft_strlen(s))
-		str_a = ft_substr(s, j + 1, ft_strlen(s));
+	*i = *i + 1;
+	while (s[*i] != c)
+		*i = *i + 1;
+	str_m = ft_substr(s, ft_strlen(str_b) + 1, *i);
+	if (*i + 1 < ft_strlen(s))
+		str_a = ft_substr(s, *i + 1, ft_strlen(s));
 // printf("\n\nstr_b = %s\nstr_m = %s\nstr_a = %s\n\n", str_b, str_m, str_a);
-	str_m = expand_str(data, str_m);
+	if (c == '"')
+		str_m = expand_str(data, str_m);
+	else
+		str_m = str_quotes_removal(str_m);
 	*i = ft_strlen(str_b) + ft_strlen(str_m);
-	s = join_all(s, str_b, str_m, str_a);
 // printf("OK\n");
-	return (s);
+	return (join_all(s, str_b, str_m, str_a));
 }
 
-static size_t	check_dollar_sign(char *s, size_t start)
+static char	*check_char(t_data *data, char *s, size_t *i, int j)
 {
-	size_t	count;
-
-	count = 0;
-	while (s[start] != '"')
+	if (j == 0) // $
 	{
-		if (s[start] == '$')
-			count++;
-		start++;
+		// if (s[*i + 1] && (ft_isalpha(s[*i + 1]) == 1 || s[*i + 1] == '?'))
+		// 	s = expand_dollar(data, s, i);
+		return (s);
 	}
-	return (count);
+	else if (j == 1) // '
+	{
+		s = expand_quotes(data, s, i, '\'');
+		return (s);
+	}
+	else
+	{
+		s = expand_quotes(data, s, i, '"');
+		return (s);
+	}
+	return (s);
 }
 
 void	expand(t_data *data) // func too long
 {
-	int		expand;
 	size_t	i;
 	t_lexer	*tmp;
 
@@ -106,29 +113,26 @@ void	expand(t_data *data) // func too long
 	while (tmp != NULL)
 	{
 		i = 0;
-		expand = 0;
 		if (tmp->word == NULL)
 			tmp = tmp->next;
 		while (tmp->word[i] != '\0')
 		{
-			if (tmp->word[i] == '\'')
-				i += quote_handling(tmp->word, i, tmp->word[i]) + 1;
+			// if (tmp->word[i] == '$')
+			// {
+			// 	tmp->word = check_char(data, tmp->word, &i, 0);
+			// }
+			// else if (tmp->word[i] == '\'')
+			// {
+			// 	tmp->word = check_char(data, tmp->word, &i, 1);
+			// }
 			if (tmp->word[i] == '"')
 			{
-				if (check_dollar_sign(tmp->word, i + 1) != 0)
-				{
-					expand = 1;
-					tmp->word = expand_d_quotes(data, tmp->word, &i);
+				tmp->word = check_char(data, tmp->word, &i, 2);
 					// if ((int)i == -1)
 					// 	return ;//ou ft_error....
-				}
-				else
-					i += quote_handling(tmp->word, i, tmp->word[i]) + 1;
 			}
 			i++;
 		}
-		if (expand == 0)
-			tmp->word = str_quotes_removal(tmp->word);
 		tmp = tmp->next;
 	}
 
--- SRCS/expander_var.c
+++ SRCS/expander_var.c
@@ -3,17 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   expander_var.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 10:50:17 by tgellon           #+#    #+#             */
-/*   Updated: 2023/04/25 09:52:02 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/04/25 16:33:03 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
 
+// char	*expand_dollar(t_data *data, char *s, size_t *i) //func too long
+// {
+// 	char	*bef;
+// 	char	*exp;
+// 	char	*aft;
+
+// 	bef = NULL;
+// 	exp = NULL;
+// 	aft = NULL;
+// 	while (s[*i] != '\0')
+// 	{
+// 		if (*i > 0)
+// 			bef = ft_substr(s, 0, *i);
+// // if (bef != NULL)
+// 	// printf("\n\n\nbef = %s c = %d\n", bef, bef[ft_strlen(bef) - 1]);
+// 		while (ft_isalnum(s[*i] == 1))
+// 			*i = *i + 1;//ou fonc si 0 alors ++ sinon break et chercher aussi $
+// 				// if (s[j] == '$' && s[j - 1] && s[j - 1] != '$')
+// 				// 	break ;
+
+
+
+
+// // printf("char stopped: %c\n", s[j]);
+// 		aft = ft_substr(s, *i, ft_strlen(s));
+// // printf("aft = %s c = %d\n", aft, aft[ft_strlen(aft) - 1]);
+// 		exp = ft_substr(s, ft_strlen(bef) + 1, *i - ft_strlen(bef) - 1);
+// // printf("exp = %s c = %d\n\n\n", exp, exp[ft_strlen(exp) - 1]);
+// 			// s = get_var()
+// 		exp = get_var(data, exp);
+// 		*i = ft_strlen(bef) + ft_strlen(exp);
+// 	}
+// 	return (join_all(s, bef, exp, aft));
+// }
+
 char	*get_var(t_data *data, char *s)
-{
+{printf("WORD PASSED: %s\n", s);
 	char	*var;
 	size_t	i;
 	size_t	k;
@@ -27,7 +62,8 @@ char	*get_var(t_data *data, char *s)
 		i = 0;
 		while (data->envp[k][i] != '=')
 			i++;
-		if (ft_strncmp(data->envp[k], s, i - 1) == 0)
+//
+		if (ft_strncmp(data->envp[k], s, i - 1) == 0 && ft_strlen(s) == i)
 		{
 			free(var);
 			var = ft_substr(data->envp[k], i + 1, (ft_strlen(*data->envp)));
