0b4515ee9f7312a447d124b8d32c62159443ad9f
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-04-28 09:36:23 +0200

    MIN-29-expand done

--- SRCS/parser.c
+++ SRCS/parser.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:52:51 by rrebois           #+#    #+#             */
-/*   Updated: 2023/04/25 09:52:55 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/04/28 09:33:40 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
 
-// void	implement_redirections_cmds(t_data *data)
-// {
-// 	files_redirection(data);
-// }
+void	create_cmds_lst(t_data *data)
+{
+	files_redirection(data);
+}
