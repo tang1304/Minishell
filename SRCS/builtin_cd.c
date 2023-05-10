5159856ca4516124c11375619ce9384735e0aa7f
Tanguy Gellon <tgellon@z4r10p3.42lyon.fr>
2023-05-10 08:39:14 +0200

    fix du segfault si enter sans rien

--- SRCS/builtin_cd.c
+++ SRCS/builtin_cd.c
@@ -6,37 +6,11 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 12:36:28 by rrebois           #+#    #+#             */
-/*   Updated: 2023/05/09 14:09:43 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/05/10 08:38:05 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
-
-// static char	*ddot_handle(t_data *data)
-// {
-// 	t_env	*tmp;
-// 	char	*new_path;
-// 	char	*pos;
-// 	size_t	len;
-// 	size_t	i;
-
-// 	tmp = data->env;
-// 	len = ft_list_size(tmp);
-// 	i = -1;
-// 	while (++i < len)
-// 	{
-// 		if (ft_strncmp(tmp->var_name, "PWD=", 4) == 0)
-// 			break ;
-// 		tmp = tmp->next;
-// 	}
-// 	pos = ft_strrchr(tmp->var_value, '/');
-// 	new_path = ft_strndup(tmp->var_value, (size_t)pos - 
-// 							(size_t)tmp->var_value - 1);
-// 	// if (!new_path)
-// 	// 	return (0);
-// 	chdir(new_path);
-// 	return (new_path);
-// }
 
 static int	cd_home(t_data *data)
 {
@@ -81,7 +55,6 @@ int	ft_cd(t_data *data, char **cmd)
 	{
 		replace_env(data, "OLDPWD=", search_env(data, "PWD="));
 		replace_env(data, "PWD=", current);
-		print_export(&data->env);
 		return (1);
 	}
 	return (perror("getcwd: "), 0);
