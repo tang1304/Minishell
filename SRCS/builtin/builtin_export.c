472cf80d59d9e905bb2dce6c55a65aebc775b198
Tanguy Gellon <tgellon@z4r10p3.42lyon.fr>
2023-05-26 08:54:35 +0200

    tri apres merge

--- SRCS/builtin/builtin_export.c
+++ SRCS/builtin/builtin_export.c
@@ -6,11 +6,40 @@
 /*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 10:07:42 by tgellon           #+#    #+#             */
-/*   Updated: 2023/05/19 15:13:34 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/05/25 11:39:56 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minishell.h"
+
+void	ft_list_sort(t_env **env, int size)
+{
+	void	*tmp_name;
+	void	*tmp_value;
+	t_env	*current;
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i < size - 1)
+	{
+		j = -1;
+		current = *env;
+		while (++j < size - 1 - i)
+		{
+			if (ft_strcmp(current->var_name, current->next->var_name) > 0)
+			{
+				tmp_name = current->var_name;
+				tmp_value = current->var_value;
+				current->var_name = current->next->var_name;
+				current->var_value = current->next->var_value;
+				current->next->var_name = tmp_name;
+				current->next->var_value = tmp_value;
+			}
+			current = current->next;
+		}
+	}
+}
 
 int	print_export(t_env **env)
 {
