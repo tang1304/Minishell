50a57a70a0882d56861e514f251ef6f2eb402379
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-06-01 15:36:00 +0200

    signals work in progress

--- SRCS/signals/signals.c
+++ SRCS/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:14:33 by rrebois           #+#    #+#             */
-/*   Updated: 2023/06/01 09:33:25 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2023/06/01 15:27:21 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	signal_set(void)
 	struct sigaction	sa;
 
 	ft_bzero(&sa, sizeof(sa));
-	// sa.sa_flags = SA_RESTART;
+	sa.sa_flags = SA_RESTART;
 	sa.sa_handler = &handler_sigint;
 	sigaction(SIGINT, &sa, NULL);
 	sa.sa_handler = SIG_IGN;
@@ -29,7 +29,21 @@ void	signal_hd_set(void)
 	struct sigaction	sa;
 
 	ft_bzero(&sa, sizeof(sa));
-	// sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = SIG_IGN;
+	sa.sa_flags = SA_RESTART;
 	sa.sa_handler = &handler_hd_sigint;
+	sigaction(SIGINT, &sa, NULL);
+}
+
+void	signal_exec_set(void)
+{
+	struct sigaction	sa;
+
+	ft_bzero(&sa, sizeof(sa));
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = SIG_IGN;
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = &handler_exec_sigint;
 	sigaction(SIGINT, &sa, NULL);
 }
