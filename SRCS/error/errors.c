c5d19a8e45825042833a73bcf0ef1eba2e27af09
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-06-05 09:15:59 +0200

    Improved free_hd_strct

--- SRCS/error/errors.c
+++ SRCS/error/errors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 14:53:32 by tgellon           #+#    #+#             */
-/*   Updated: 2023/06/02 10:04:28 by tgellon          ###   ########lyon.fr   */
+/*   Updated: 2023/06/05 08:59:28 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	exit_error(t_data *data)
 	free_all(data);
 	close(data->pipe[0]);
 	close(data->pipe[1]);
-	close_files(data);
+	close_all(data);//changed fro; close_files to close all
 	exit(EXIT_FAILURE);
 }
