b6d5704a3fd4a2dbcba893e3b0d98c172c2050d5
Tanguy Gellon <tgellon@z4r10p5.42lyon.fr>
2023-06-09 15:41:23 +0200

    fix apres merge

--- SRCS/expand/expand_string.c
+++ SRCS/expand/expand_string.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand_string.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 12:01:24 by rrebois           #+#    #+#             */
-/*   Updated: 2023/06/09 15:04:30 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2023/06/09 15:40:31 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,31 +37,31 @@ void	string_xpd_hd(t_data *data, t_substr *s, size_t *i, char *buffer)
 	s->s = join_all_sub(data, s->s, s);//check leaks here
 }
 
-void	string_xpd(t_data *data, t_substr *s, size_t *i, size_t index)
-{
-	int	err;
+// void	string_xpd(t_data *data, t_substr *s, size_t *i, size_t index)
+// {
+// 	int	err;
 
-	err = 0;
-	if (*i > 1)
-	{
-		s->sub_b = ft_substr_check(s->s, 0, *i - 1, &err);
-		if (err > 0)
-			expand_error(data, s, "minishell: malloc_error: ");
-	}
-	while (ft_isalnum(s->s[*i]) == 1)
-		*i = *i + 1;
-	s->sub_a = ft_substr_check(s->s, *i, ft_strlen(s->s) - *i + 1, &err);
-	if (err > 0)
-			expand_error(data, s, "minishell: malloc_error: ");
-	data->buf = ft_substr_check(s->s, ft_strlen(s->sub_b), \
-	*i - (ft_strlen(s->sub_b)), &err);
-	if (err > 0)
-			expand_error(data, s, "minishell: malloc_error: ");
-	s->sub_m = get_var(data, data->buf, &err);
-	if (err > 0)
-			expand_error(data, s, "minishell: malloc_error: ");
-	if (check_space_expand(data, s, index) == 1)
-		return ;
-	*i = ft_strlen(s->sub_b) + ft_strlen(s->sub_m);
-	s->s = join_all_sub(data, s->s, s);//check leaks here ->prob not
-}
+// 	err = 0;
+// 	if (*i > 1)
+// 	{
+// 		s->sub_b = ft_substr_check(s->s, 0, *i - 1, &err);
+// 		if (err > 0)
+// 			expand_error(data, s, "minishell: malloc_error: ");
+// 	}
+// 	while (ft_isalnum(s->s[*i]) == 1)
+// 		*i = *i + 1;
+// 	s->sub_a = ft_substr_check(s->s, *i, ft_strlen(s->s) - *i + 1, &err);
+// 	if (err > 0)
+// 			expand_error(data, s, "minishell: malloc_error: ");
+// 	data->buf = ft_substr_check(s->s, ft_strlen(s->sub_b), \
+// 	*i - (ft_strlen(s->sub_b)), &err);
+// 	if (err > 0)
+// 			expand_error(data, s, "minishell: malloc_error: ");
+// 	s->sub_m = get_var(data, data->buf, &err);
+// 	if (err > 0)
+// 			expand_error(data, s, "minishell: malloc_error: ");
+// 	if (check_space_expand(data, s, index) == 1)
+// 		return ;
+// 	*i = ft_strlen(s->sub_b) + ft_strlen(s->sub_m);
+// 	s->s = join_all_sub(data, s->s, s);//check leaks here ->prob not
+// }
