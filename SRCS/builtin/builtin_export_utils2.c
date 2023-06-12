d06b564b2f0555e9a43f9f3f33c274aaea2253b2
tang1304 <tanguy.gellon@gmail.com>
2023-06-12 11:07:10 +0200

    fix sur export

--- SRCS/builtin/builtin_export_utils2.c
+++ SRCS/builtin/builtin_export_utils2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export_utils2.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: tgellon <tgellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 15:11:44 by rrebois           #+#    #+#             */
-/*   Updated: 2023/06/08 15:23:05 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2023/06/12 10:58:54 by tgellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,6 @@ void	iterate_pp_envp(t_data *data, char *cmd, int i, int export)
 		{
 			envp_replacement(data, &data->envp[j], cmd);
 			export += 1;
-			break ;
-		}
-		else if (export == 1 && ft_strchr(cmd, '='))
-		{
-			data->envp = export_var_only(data, cmd);
 			break ;
 		}
 	}
