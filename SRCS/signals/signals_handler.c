49e5eef19699e6f527ccfa0af8887682f2ef0b6e
Rolando Rebois <rrebois@z3r3p3.42lyon.fr>
2023-06-13 13:09:54 +0200

    Project finished

--- SRCS/signals/signals_handler.c
+++ SRCS/signals/signals_handler.c
@@ -6,7 +6,7 @@
 /*   By: rrebois <rrebois@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 09:11:24 by rrebois           #+#    #+#             */
-/*   Updated: 2023/06/13 12:05:38 by rrebois          ###   ########lyon.fr   */
+/*   Updated: 2023/06/13 12:19:30 by rrebois          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,8 @@ void	handler_hd_sigint(int signal)
 void	handler_exec(int signal)
 {
 	if (signal == 2)
-	{
 		write(1, "\n", 1);
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-	}
 	else
-	{
 		printf("Quit (core dumped)\n");
-	}
 	g_status = 128 + signal;
 }
-
-// void	handler_exec_sigquit(int signal)
-// {printf("ici\n");
-// 	write(1, "\n", 1);
-// 	rl_replace_line("Quit (Core dumped)", 0);
-// 	g_status = 128 + signal;
-// }
